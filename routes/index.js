97345d4061e4129a1f1f374af69e8eb32f9bf665
Daehyun Kim <daehyun@uangel.com>
2019-12-26 18:00:20 +0900

    routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,10 @@ router.get('/', userController.basicAPI);
 router.get('/test', userController.testAPI);
 router.post('/test', userController.postTestAPI);
 
+router.get('/download', function (req, res, next) {
+    var filePath = "C:/Users/user/git/earth/pdf/1/manual1-1.pdf"; 
+
+    res.download(filePath);    
+});
+
 module.exports = router;
